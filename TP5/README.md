3265609a5b5129e789b70f2c93394df92203b96d
Tibor Stanko <tibor.stanko@gmail.com>
2017-03-08 23:57:18 +0100

    TP5

---
+++ TP5/README.md
@@ -0,0 +1,60 @@
+## TP5 : Lane-Riesenfeld algorithm
+In this TP, you'll implement the classical Lane-Riesenfeld algorithm, as well as its six-point variant.
+
+```bash
+cd GeoNum2017/
+git pull
+```
+or, if you don't have the local repo
+```bash
+git clone https://github.com/GeoNumTP/GeoNum2017.git
+```
+Then
+```
+cd TP5/
+python tp5.py
+```
+
+As before, you can pass parameters directly as command line args:
+```bash
+python tp5.py  [data=hepta; bone,infinity,sumsign]  [scheme=LR; SP]  [curve degree]  [subdivision depth]
+```
+`LR` stands for Lane-Riesenfeld algorithm, `SP` is the six-point scheme.
+
+For instance, to subdivide the `bone` dataset with degree 3 six-point:
+```bash
+python tp5.py sumsign SP 3
+```
+
+### Functions to modify
+* `LaneRiesenfeld` : perform one iteration of the Lane-Riesenfeld algorithm.
+* `SixPoint` : perform one iteration of six-point variant of LR.
+
+### Lane-Riesenfeld
+The Lane-Riesenfeld algorithm is a subdivision scheme which serves for efficient evaluation of uniform B-splines.
+As for the subdivision schemes we've seen in the previous TP,
+two steps are needed to compute the subdivided polygon `X1` from the base polygon `X0`.
+```python
+n = X0.shape[0]
+X1 = np.zeros([2*n,2])
+```
+In the topological step, the Lane-Riesenfeld scheme doubles the amount of points by taking each initial point twice.
+```
+P0 P0 P1 P1 ... Pn Pn
+```
+This sequence is then refined k times via midpoint averaging 0.5*(A+B),
+with k being the degree of the curve (user-defined).
+For k=2:
+```
+  ...P0    P0   P1    P1    P2    P2    P3    P3 
+      \   / \  / \   / \   / \   / \   / \   / \
+   ... P00   P01   P11   P12   P22   P23   P33 ...
+        \   / \   / \   / \   / \   / \   / \
+     ... P001  P011  P112  P122  P223  P233 ...
+```
+
+### ToDo
+1. Implement the two subdivision schemes. Test with the provided datasets.
+1. Try varying the degree parameter. How do the curves change?
+1. What differences between the two schemes do you observe?  
+In your opinion, which scheme gives better results? Why?
---
+++ TP5/tp5.py
@@ -0,0 +1,213 @@
+#------------------------------------------------------
+#
+#  TP5 : Lane-Riesenfeld algorithm
+#  http://tiborstanko.sk/teaching/geo-num-2017/tp5.html
+#  [10-Mar-2017]
+#
+#------------------------------------------------------
+#
+#  This file is a part of the course:
+#    Geometrie numerique (spring 2017)
+#    https://github.com/GeoNumTP/GeoNum2017
+#    M1 Informatique
+#    UFR IM2AG
+#
+#  Course lecturer:
+#    Georges-Pierre.Bonneau at inria.fr
+#
+#  Practical part:
+#    Tibor.Stanko at inria.fr
+#
+#------------------------------------------------------
+
+import sys, os
+import matplotlib.pyplot as plt
+import numpy as np
+
+TP = os.path.dirname(os.path.realpath(__file__)) + "/"
+DATADIR = filename = TP+"data/"
+
+
+#-------------------------------------------------
+# READDATAPOINTS()
+# Read datapoints from a file.
+#
+# Input
+#    filename :  file to be read
+#
+# Output
+#    DataPts  :  d x 2 matrix, represents a closed polygon
+#
+def ReadDatapoints( filename ) :
+    datafile = open(filename,'r');
+    p = np.fromstring(datafile.readline(),sep=' ',dtype=int)
+    DataPts = np.fromfile(datafile,count=2*p,sep=' ',dtype=float)
+    DataPts = DataPts.reshape(-1,2)
+    return DataPts
+
+
+#-------------------------------------------------
+# LANERIESENFELD()
+# Perform one iteration of the Lane-Riesenfeld algorithm.
+#
+# Input
+#    X0       :  n x 2 matrix, initial polygon
+#    degree   :  degree of subdivision
+#
+# Output
+#    X1       :  2n x 2 matrix, subdivided polygon
+#
+#
+# reference paper:
+#   Lane, Riesenfeld
+#   A Theoretical Development for the Computer Generation
+#    and Display of Piecewise Polynomial Surfaces
+#   1980. IEEE Transactions on Pattern Analysis and Machine Intelligence
+#   https://doi.org/10.1109/TPAMI.1980.4766968
+#
+#
+def LaneRiesenfeld(X0,degree) :
+    
+    # number of points
+    n = X0.shape[0]
+    
+    # upsample
+    X1 = np.zeros([2*n,2])
+        
+    ##
+    ## TODO Implement the Lane-Riesenfeld algorithm.
+    ##
+
+    return X1
+
+
+#-------------------------------------------------
+# SIXPOINT()
+# Perform one iteration of the six-point subdivision.
+# 
+# Input
+#    X0       :  n x 2 matrix, initial polygon
+#    degree   :  degree of subdivision
+#
+# Output
+#    X1       :  2n x 2 matrix, subdivided polygon
+#
+#
+# reference paper:
+#   Ashraf et al.
+#   A Six-Point Variant on the Lane-Riesenfeld Algorithm
+#   2014. Journal of Applied Mathematics
+#   http://dx.doi.org/10.1155/2014/628285
+#
+#
+def SixPoint(X0,degree) :
+    
+    # number of points
+    n = X0.shape[0]
+    
+    # upsample
+    X1 = np.zeros([2*n,2])
+    
+    ##
+    ## TODO Implement the six-point scheme.
+    ##
+    
+    return X1
+
+
+#-------------------------------------------------
+if __name__ == "__main__":
+    
+    #---------------------------------------------
+    # arg 1 : data name 
+    if len(sys.argv) > 1 :
+        dataname = sys.argv[1]
+    else :
+        dataname = "hepta"
+        
+    #---------------------------------------------    
+    # arg 2 : scheme name
+    if len(sys.argv) > 2 :
+        scheme = sys.argv[2]
+    else :
+        scheme = "LR"
+    # check
+    if scheme == "LR" :
+        schemeName = "Lane-Riesenfeld"
+    elif scheme == "SP" :
+        schemeName = "six-point scheme"
+    else :            
+        print " error :  invalid scheme "+scheme
+        print "          should be LR or SP"
+        sys.exit(0)
+        
+    #---------------------------------------------
+    # arg 3 : degree of the curve
+    if len(sys.argv) > 3 :
+        degree = int(sys.argv[3])
+    else :
+        degree = 3
+        
+    #---------------------------------------------
+    # arg 4 : number of subdivisions
+    if len(sys.argv) > 4 :
+        subdivisions = int(sys.argv[4])
+    else :
+        subdivisions = 5
+        
+    # filename
+    filename = DATADIR + dataname + ".data"
+    
+    # check if valid datafile
+    if not os.path.isfile(filename) :
+        print " error :  invalid dataname '" + dataname + "'"
+        print " usage :  python tp5.py  [data=hepta; bone,infinity,sumsign]  [scheme=LR; SP]  [degree=3]  [subdivisions=5]"
+        
+    else :
+        
+        # read data
+        P = ReadDatapoints(filename)    
+        
+        # init subdivided polygon
+        X = P
+        
+        # iterative subdivision
+        for i in range(subdivisions) :
+            
+            # Lane-Riesenfeld
+            if scheme == "LR" :
+                X = LaneRiesenfeld(X,degree)
+                
+            # Six-point scheme
+            else :
+                X = SixPoint(X,degree)
+        
+        # set axes with equal proportions
+        plt.axis('equal')
+        
+        # plot the data
+        plt.cla()
+        
+        # input polygon
+        plt.fill( P[:,0], P[:,1], edgecolor=.33*np.ones(3), linewidth=1, linestyle='--', fill=False)
+    
+        # refined polygon
+        plt.fill( X[:,0], X[:,1], edgecolor='b', linestyle='-', linewidth=2, fill=False )
+            
+        # titles
+        # plot
+        ptitle  = dataname+" : "
+        ptitle += schemeName +", "
+        ptitle += "deg="+str(degree)+", "
+        ptitle += "sub="+str(subdivisions)
+        plt.title(ptitle)
+        # figure
+        plt.gcf().canvas.set_window_title('TP5 Lane-Riesenfeld')
+
+        ##
+        ## TODO : Uncomment if you want to save the render as png image in data/ folder
+        ##
+        #plt.savefig( DATADIR+dataname+".png" )
+        
+        # render
+        plt.show()        
