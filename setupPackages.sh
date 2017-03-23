513639134f26c5246ecb4b27879baa1ae2320994
Tibor Stanko <stankoti@f216-16.e-im2ag.ujf-grenoble.fr>
2017-03-23 18:00:34 +0100

    modif setupPackages.sh

--- setupPackages.sh
+++ setupPackages.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 clear
+. ./exportPath.sh
+echo "==============================="
+echo "PYTHONPATH set to $PYTHONPATH"
 
 #-------------------------------------------------------------------------------
 # Create local package folder
@@ -12,60 +15,76 @@ fi
 echo "==============================="
 
 #-------------------------------------------------------------------------------
-# PyOpenGL
+# PyOpenGL$(pwd)
 #-------------------------------------------------------------------------------
-wget "https://pypi.python.org/packages/source/P/PyOpenGL/PyOpenGL-3.1.1a1.tar.gz" -q --show-progress
-echo "-------------------------------"
-echo "Extract PyOpenGL-3.1.1a1.tar.gz"
-tar xfz PyOpenGL-3.1.1a1.tar.gz
-echo "-------------------------------"
-echo "Install PyOpenGL-3.1.1a1"
-cd PyOpenGL-3.1.1a1
-python setup.py -q install --prefix=$(pwd)/..
-cd ..
+if [ ! -d "$(pwd)/lib/python2.7/site-packages/PyOpenGL-3.1.1a1-py2.7.egg/" ]; then
+    wget "https://pypi.python.org/packages/source/P/PyOpenGL/PyOpenGL-3.1.1a1.tar.gz" -q --show-progress
+    echo "-------------------------------"
+    echo "Extract PyOpenGL-3.1.1a1.tar.gz"
+    tar xfz PyOpenGL-3.1.1a1.tar.gz
+    echo "-------------------------------"
+    echo "Install PyOpenGL-3.1.1a1"
+    cd PyOpenGL-3.1.1a1
+    python setup.py -q install --prefix=$(pwd)/..
+    cd ..    
+    echo "-------------------------------"
+    echo "Clean"
+    rm -f PyOpenGL-3.1.1a1.tar.gz
+    rm -rf PyOpenGL-3.1.1a1/
+else
+    echo "PyOpenGL is already installed in lib/python2.7/site-packages/"
+    echo "Skipping..."
+fi
 echo "==============================="
 
 #-------------------------------------------------------------------------------
 # PyGLFW
 #-------------------------------------------------------------------------------
-wget "https://pypi.python.org/packages/01/d8/155dc626d01f2b267ced7196378da77818f0560c9471ef9cdf9f02bd8611/glfw-1.3.3.tar.gz" -q --show-progress
-echo "-------------------------------"
-echo "Extract glfw-1.3.3.tar.gz"
-tar xzf glfw-1.3.3.tar.gz
-echo "-------------------------------"
-echo "Install glfw-1.3.3"
-cd glfw-1.3.3
-python setup.py -q install --prefix=$(pwd)/..
-cd ..
+if [ ! -f "$(pwd)/lib/python2.7/site-packages/glfw.py" ]; then
+    wget "https://pypi.python.org/packages/01/d8/155dc626d01f2b267ced7196378da77818f0560c9471ef9cdf9f02bd8611/glfw-1.3.3.tar.gz" -q --show-progress
+    echo "-------------------------------"
+    echo "Extract glfw-1.3.3.tar.gz"
+    tar xzf glfw-1.3.3.tar.gz
+    echo "-------------------------------"
+    echo "Install glfw-1.3.3"
+    cd glfw-1.3.3
+    python setup.py -q install --prefix=$(pwd)/..
+    cd ..
+    echo "-------------------------------"
+    echo "Clean"
+    rm -f PyOpenGL-3.1.1a1.tar.gz
+    rm -rf PyOpenGL-3.1.1a1/
+else
+    echo "PyGLFW is already installed in lib/python2.7/site-packages/"
+    echo "Skipping..."
+fi
 echo "==============================="
 
 #-------------------------------------------------------------------------------
 # GLFW
 #-------------------------------------------------------------------------------
-wget "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.zip" -q --show-progress
-echo "-------------------------------"
-echo "Extract glfw-3.2.1.zip"
-unzip -qqo glfw-3.2.1.zip
-echo "-------------------------------"
-echo "Compile libglfw.so"
-mkdir glfw-3.2.1/build
-cd glfw-3.2.1/build
-cmake -DBUILD_SHARED_LIBS=ON ..
-make -j 4 -s
-cd ../../
-echo "-------------------------------"
-echo "copy libglfw.so* to basedir"
-for file in glfw-3.2.1/build/src/libglfw.so*; do cp "$file" .;done
+if [ ! -f "$(pwd)/libglfw.so" ]; then
+    wget "https://github.com/glfw/glfw/releases/download/3.2.1/glfw-3.2.1.zip" -q --show-progress
+    echo "-------------------------------"
+    echo "Extract glfw-3.2.1.zip"
+    unzip -qqo glfw-3.2.1.zip
+    echo "-------------------------------"
+    echo "Compile libglfw.so"
+    mkdir glfw-3.2.1/build
+    cd glfw-3.2.1/build
+    cmake -DBUILD_SHARED_LIBS=ON ..
+    make -j 4 -s
+    cd ../../
+    echo "-------------------------------"
+    echo "copy libglfw.so* to basedir"
+    for file in glfw-3.2.1/build/src/libglfw.so*; do cp "$file" .;done
+    echo "-------------------------------"
+    echo "Clean"
+    rm -f glfw-3.2.1.zip
+    rm -rf glfw-3.2.1/
+else
+    echo "libglfw.so is already installed in ."
+    echo "Skipping..."
+fi
 echo "==============================="
 
-#-------------------------------------------------------------------------------
-# clean
-#-------------------------------------------------------------------------------
-echo "clean"
-rm -f PyOpenGL-3.1.1a1.tar.gz
-rm -f glfw-1.3.3.tar.gz
-rm -f glfw-3.2.1.zip
-rm -rf PyOpenGL-3.1.1a1/
-rm -rf glfw-1.3.3/
-rm -rf glfw-3.2.1/
-echo "==============================="
